950b6d9a3cf0ce8ac6b2f464c8a55dd7d4b076f0
vgraul <vincent.graul@outlook.com>
2020-06-14 22:02:05 +0200

    feat: add separator component

---
+++ src/components/separator/Separator.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import Breakpoints from "../../breakpoints";
+
+const Separator = styled.img`
+  margin: ${Breakpoints.isTabletOrLower()
+    ? Breakpoints.isPortrait()
+      ? "6vh 0"
+      : "6vw 0"
+    : "6vh 0"};
+  align-self: center;
+  width: ${Breakpoints.isTabletOrLower()
+    ? `${Breakpoints.isPortrait() ? "20vw" : "10vw"}`
+    : "5vw"};
+`;
+
+export default Separator;
