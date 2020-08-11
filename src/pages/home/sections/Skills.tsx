d7ed717a28e01eed1537c101a7b13f1b699f3b9c
vgraul <vincent.graul@outlook.com>
2020-08-11 14:24:31 +0200

    refactor: add mobile section

--- src/pages/home/sections/Skills.tsx
+++ src/pages/home/sections/Skills.tsx
@@ -6,61 +6,73 @@ import colors from "../../../colors";
 import Breakpoints from "../../../breakpoints";
 
 export default function Skills(): JSX.Element {
-  return (
-    <Section
-      card={{
-        renderHeader: () => <HeaderWithTitle title="Mes compétences" />,
-        children: (
-          <Container>
-            <Line>
-              <Label>Langages</Label>
-              <TagContainer>
-                <Tag>TypeScript</Tag>
-                <Tag>JavaScript</Tag>
-                <Tag>HTML</Tag>
-                <Tag>CSS</Tag>
-              </TagContainer>
-            </Line>
+    return (
+        <Section
+            card={{
+                renderHeader: () => <HeaderWithTitle title="Mes compétences" />,
+                children: (
+                    <Container>
+                        <Line>
+                            <Label>Langages</Label>
+                            <TagContainer>
+                                <Tag>TypeScript</Tag>
+                                <Tag>JavaScript</Tag>
+                                <Tag>HTML</Tag>
+                                <Tag>CSS</Tag>
+                            </TagContainer>
+                        </Line>
 
-            <Line>
-              <Label>
-                Technologies
-                {Breakpoints.isTabletOrLower() ? " " : <br />}
-                front-end
-              </Label>
-              <TagContainer>
-                <Tag>React</Tag>
-                <Tag>Redux</Tag>
-                <Tag>Styled-Components</Tag>
-                <Tag>Flexbox</Tag>
-              </TagContainer>
-            </Line>
+                        <Line>
+                            <Label>
+                                Technologies
+                                {Breakpoints.isTabletOrLower() ? " " : <br />}
+                                front-end
+                            </Label>
+                            <TagContainer>
+                                <Tag>React</Tag>
+                                <Tag>Redux</Tag>
+                                <Tag>Styled-Components</Tag>
+                                <Tag>Flexbox</Tag>
+                            </TagContainer>
+                        </Line>
 
-            <Line>
-              <Label>
-                Technologies
-                {Breakpoints.isTabletOrLower() ? " " : <br />}
-                back-end
-              </Label>
-              <TagContainer>
-                <Tag>Node</Tag>
-                <Tag>Nest</Tag>
-                <Tag>Express</Tag>
-              </TagContainer>
-            </Line>
-          </Container>
-        ),
-        noBorder: Breakpoints.isTabletOrLower(),
-        noRadius: Breakpoints.isTabletOrLower(),
-      }}
-      orientation={Breakpoints.isTabletOrLower() ? undefined : Orientation.LEFT}
-      renderImage={() =>
-        Breakpoints.isTabletOrLower() ? null : (
-          <Image src="/shapes/blocks.svg" />
-        )
-      }
-    ></Section>
-  );
+                        <Line>
+                            <Label>
+                                Technologies
+                                {Breakpoints.isTabletOrLower() ? " " : <br />}
+                                back-end
+                            </Label>
+                            <TagContainer>
+                                <Tag>Node</Tag>
+                                <Tag>Nest</Tag>
+                                <Tag>Express</Tag>
+                            </TagContainer>
+                        </Line>
+
+                        <Line>
+                            <Label>
+                                Technologies
+                                {Breakpoints.isTabletOrLower() ? " " : <br />}
+                                mobiles
+                            </Label>
+                            <TagContainer>
+                                <Tag>React Native</Tag>
+                                <Tag>Expo</Tag>
+                            </TagContainer>
+                        </Line>
+                    </Container>
+                ),
+                noBorder: Breakpoints.isTabletOrLower(),
+                noRadius: Breakpoints.isTabletOrLower(),
+            }}
+            orientation={Breakpoints.isTabletOrLower() ? undefined : Orientation.LEFT}
+            renderImage={() =>
+                Breakpoints.isTabletOrLower() ? null : (
+                    <Image src="/shapes/blocks.svg" />
+                )
+            }
+        ></Section>
+    );
 }
 
 const Container = styled.div`
@@ -93,10 +105,11 @@ const Tag = styled.span`
   display: inline-block;
   padding: 0.5rem;
   background-color: ${colors.primary};
+  font-weight: bold;
   color: ${colors.light.primary};
   border: ${Breakpoints.isTabletOrLower()
-      ? `${Breakpoints.isPortrait() ? "0.9vw" : "0.6vw"}`
-      : "0.2vw"}
+    ? `${Breakpoints.isPortrait() ? "0.9vw" : "0.6vw"}`
+    : "0.2vw"}
     dashed black;
   margin: ${Breakpoints.isTabletOrLower()
     ? `0 2vw ${Breakpoints.isPortrait() ? "2vh" : "2vw"} 0`
