0c0353f447f5f796f837f481830c21ab6a81cc89
vgraul <vincent.graul@outlook.com>
2020-08-11 14:09:25 +0200

    refactor: remove redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import { rootReducer } from "./store/index";
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
