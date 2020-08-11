2d14e962c341fe347c97bf9e2d240305c507c722
vgraul <vincent.graul@outlook.com>
2020-08-11 14:42:38 +0200

    refactor: change font size

--- src/pages/home/sections/Header.tsx
+++ src/pages/home/sections/Header.tsx
@@ -4,15 +4,15 @@ import colors from "../../../colors";
 import Breakpoints from "../../../breakpoints";
 
 export default function Header(): JSX.Element {
-  return (
-    <Container>
-      <Title>
-        Vincent Graul <br /> Développeur Front-end <br />{" "}
-        <TextBackground>React</TextBackground>
-      </Title>
-      <BlocksLine src="/shapes/blocks-line.svg" />
-    </Container>
-  );
+    return (
+        <Container>
+            <Title>
+                Vincent Graul <br /> Développeur Front-end <br />{" "}
+                <TextBackground>React</TextBackground>
+            </Title>
+            <BlocksLine src="/shapes/blocks-line.svg" />
+        </Container>
+    );
 }
 
 const Container = styled.div`
@@ -27,19 +27,20 @@ const TextBackground = styled.span`
   color: ${colors.light.primary};
   padding: 0.5vh ${Breakpoints.isTabletOrLower() ? "3vw" : "1vw"};
   display: inline-block;
+  font-weight: 800;
   margin-top: ${Breakpoints.isTabletOrLower()
     ? `${Breakpoints.isPortrait() ? "4vh" : "4vw"}`
     : "4vw"};
   border: ${Breakpoints.isTabletOrLower()
-      ? `${Breakpoints.isPortrait() ? "1.4vw" : "1.0vw"}`
-      : "0.5vw"}
+    ? `${Breakpoints.isPortrait() ? "1.4vw" : "1.0vw"}`
+    : "0.5vw"}
     dashed black;
 `;
 
 const Title = styled.h1`
   font-size: ${Breakpoints.isTabletOrLower()
-    ? `${Breakpoints.isPortrait() ? "14vw" : "11vw"}`
-    : "7vw"};
+    ? `${Breakpoints.isPortrait() ? "16vw" : "12vw"}`
+    : "8vw"};
   align-self: flex-start;
   margin: ${Breakpoints.isTabletOrLower() ? "0" : "3vw 0 3vw 3vw"};
 `;
