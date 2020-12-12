260f80099885f2cbf1beaa2eb30c6287bc284ffe
Vincent Graul <vincent.graul@outlook.com>
2020-12-12 14:57:39 +0100

    refactor: change Section style

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -1,50 +1,26 @@
-import React, { ReactElement } from "react";
+import React, { Fragment, ReactNode } from "react";
 import styled from "styled-components";
-import Card, { Props as CardProps } from "../card/Card";
+import Separator from "../separator/Separator";
 
-export enum Orientation {
-    LEFT = "left",
-    RIGHT = "right",
-}
-
-interface Props {
-    card: CardProps;
-    orientation?: Orientation;
-    renderImage: () => ReactElement | null;
-    className?: string;
+export interface Props {
+  children?: ReactNode;
+  separator?: boolean;
+  className?: string;
 }
 
 export default function Section(props: Props): JSX.Element {
-    const { card, orientation, renderImage, className } = props;
+  const { className, children, separator } = props;
 
-    return (
-        <Container className={`${className}`} orientation={orientation}>
-            {renderImage()}
-            <SectionCard
-                {...card}
-                orientation={orientation}
-                hasImage={renderImage() !== null}
-            ></SectionCard>
-        </Container>
-    );
+  return (
+    <Fragment>
+      <Container className={`${className}`}>{children}</Container>
+      {separator ? <Separator src="/shapes/triangle.svg" /> : null}
+    </Fragment>
+  );
 }
 
-const Container = styled.div<{ orientation?: Orientation }>`
+const Container = styled.div`
   display: flex;
-  justify-content: space-between;
-  flex-direction: ${(props) =>
-    props.orientation === Orientation.LEFT ? "row-reverse" : "row"};
-`;
-
-const SectionCard = styled(Card)<{
-    orientation?: Orientation;
-    hasImage: boolean;
-}>`
-  width: ${(props) => (props.hasImage ? "80%" : "100%")};
-  margin: ${(props) => {
-    if (!props.hasImage) {
-        return "0";
-    }
-    return props.orientation === Orientation.LEFT ? "0 0 0 2rem" : "0 2rem 0 0";
-}};
+  flex-flow: column;
+  align-items: center;
 `;
