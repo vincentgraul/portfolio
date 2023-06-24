17906301d2db6b3c04fb0208f19971fa8850bcbd
Vincent Graul <vincent.graul@outlook.com>
2023-06-24 17:40:02 +0200

    refactor: add contact button

--- pages/home/sections/About.tsx
+++ pages/home/sections/About.tsx
@@ -1,35 +1,35 @@
 import React, { useContext } from "react";
-import styled, { ThemeContext } from "styled-components";
+import styled, { ThemeContext, css } from "styled-components";
 import Section from "../../../components/section/Section";
 import Button from "../../../components/button/Button";
 import Card from "../../../components/card/Card";
 import { Theme } from "../../_app";
+import { useModalStore } from "../../../store/modal";
+import ContactModal from "../../../components/modal/ContactModal";
 
 export default function About(): JSX.Element {
   const { resolution } = useContext<Theme>(ThemeContext);
+  const { showModal } = useModalStore();
 
   return (
     <Section animation>
-      <Card
-        full={resolution.isTabletOrLower}
-        radius={resolution.isLaptopOrUpper}
-      >
+      <Card full={resolution.isTabletOrLower} radius={resolution.isLaptopOrUpper}>
         <Text>
           Bonjour, je suis Vincent Graul. <br /> <br />
-          Je suis passionné par le développement web et d’applications mobiles.
-          J’aime me lancer des défis, me remettre en question et concrétiser les
-          projets dans lesquels je m’investis, qu’ils soient personnels ou
-          professionnels. <br />
-          Dans ma vie de développeur j’accorde une place importante à la veille,
-          aux bonnes pratiques ainsi qu’aux tests logiciels. <br />
+          Je suis passionné par le développement web et d’applications mobiles. J’aime me lancer des
+          défis, me remettre en question et concrétiser les projets dans lesquels je m’investis,
+          qu’ils soient personnels ou professionnels. <br />
+          Dans ma vie de développeur j’accorde une place importante à la veille, aux bonnes
+          pratiques ainsi qu’aux tests logiciels. <br />
           <br />
-          Pour finir, j’aime échanger sur ma passion et mon expérience que ce
-          soit entre collègues, avec des personnes novices, ou autres…
+          Pour finir, j’aime échanger sur ma passion et mon expérience que ce soit entre collègues,
+          avec des personnes novices, ou autres…
         </Text>
 
-        <a href="/cv.pdf" target="_blank">
-          <CVButton>Voir mon CV</CVButton>
-        </a>
+        <ButtonContainer>
+          <CVButton onClick={() => window.open("/cv.pdf", "_blank")}>Voir mon CV</CVButton>
+          <ContactButton onClick={() => showModal(ContactModal)}>Me contacter</ContactButton>
+        </ButtonContainer>
       </Card>
     </Section>
   );
@@ -50,21 +50,40 @@ word-spacing: 0.5vw;
 `}
 `;
 
-const CVButton = styled(Button)`
+const ButtonContainer = styled.div`
   ${({ theme }) => `
-margin-top: 3vw;
-font-size: 1.5vw;
-align-self: flex-start;
+    display: flex;
+    margin-top: 3vw;
 
-@media ${theme.breakpoints.tabletOrLower} {
-  font-size: 3vw;
-  margin-top: 8vw;
-  align-self: center;
-
-  @media ${theme.breakpoints.portrait} {
-    font-size: 6vw;
-    margin-top: 8vh;
-  }
-}
+    @media ${theme.breakpoints.tabletOrLower} {
+      margin-top: 8vw;
+    
+      @media ${theme.breakpoints.portrait} {
+        margin-top: 8vh;
+      }
+    }
 `}
+`;
+
+const ButtonStyles = css`
+  ${({ theme }) => `
+    font-size: 1.5vw;
+
+    @media ${theme.breakpoints.tabletOrLower} {
+      font-size: 3vw;
+
+    @media ${theme.breakpoints.portrait} {
+      font-size: 6vw;
+    }
+  }
+`}
+`;
+
+const CVButton = styled(Button)`
+  ${ButtonStyles}
+`;
+
+const ContactButton = styled(Button)`
+  ${ButtonStyles}
+  margin-left: 3vw;
 `;
